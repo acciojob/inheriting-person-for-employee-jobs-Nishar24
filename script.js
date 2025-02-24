ad56799d1fd9399ced38da48b16694ed541135a5
Nishar24 <mdnishar75640@gmail.com>
2025-02-25 00:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,9 +23,12 @@ class Employee extends Person {
     }
 }
 
-// Example Usage
+/*// Example Usage
 const person = new Person("Alice", 25);
 person.greet(); // Expected Output: Hello, my name is Alice, I am 25 years old.
 
 const employee = new Employee("Bob", 30, "Manager");
 employee.jobGreet(); // Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
+*/
+window.Person = Person
+window.Employee = Employee
